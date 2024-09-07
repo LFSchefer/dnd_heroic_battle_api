cb611f81eb2fa3da752ea277df33884748eea3b2
louis frederic schefer <lf.schefer@gmail.com>
2024-09-07 20:12:11 +0200

    update battleupdate dto

--- src/main/java/co/simplon/dnd_heroic_battle_api/dtos/battle/BattleUpdate.java
+++ src/main/java/co/simplon/dnd_heroic_battle_api/dtos/battle/BattleUpdate.java
@@ -3,9 +3,10 @@ package co.simplon.dnd_heroic_battle_api.dtos.battle;
 import co.simplon.dnd_heroic_battle_api.dtos.battle.validators.UniqueBattleNameUpdate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 @UniqueBattleNameUpdate
-public record BattleUpdate(@Positive Long id, @NotBlank @Size(min = 5) String battleName, @Positive int turn) {
+public record BattleUpdate(@Positive Long id, @NotBlank @Size(min = 5, max = 50) String battleName, @PositiveOrZero int turn) {
 
 }

c6e0f5d127d1477f57b9ec2bebd4e2cf2f602cd7
LFSchefer <lf.schefer@gmail.com>
2025-07-09 20:12:06 +0200

    add profil

--- src/main/java/co/simplon/dnd_heroic_battle_api/controllers/UserController.java
+++ src/main/java/co/simplon/dnd_heroic_battle_api/controllers/UserController.java
@@ -1,24 +1,18 @@
 package co.simplon.dnd_heroic_battle_api.controllers;
 
-import java.nio.file.AccessDeniedException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import co.simplon.dnd_heroic_battle_api.dtos.user.Tokens;
 import co.simplon.dnd_heroic_battle_api.dtos.user.UserCreateDto;
 import co.simplon.dnd_heroic_battle_api.dtos.user.UserLoginDto;
 import co.simplon.dnd_heroic_battle_api.dtos.user.UserView;
 import co.simplon.dnd_heroic_battle_api.services.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.security.auth.message.AuthException;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api/users")
@@ -38,10 +32,10 @@ public class UserController {
     public UserView get(@Valid @RequestBody UserLoginDto input) {
         return userService.login(input);
     }
-    
+
     @PostMapping("/token-renewal")
     @ResponseStatus(code = HttpStatus.CREATED)
     public Tokens tokenRenewal(@RequestBody Tokens tokens) throws JsonProcessingException, AccessDeniedException, AuthException {
-    	return userService.renewalToken(tokens);
+        return userService.renewalToken(tokens);
     }
 }
